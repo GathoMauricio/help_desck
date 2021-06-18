8d39df94e73558aea60186d09269c9ada9bae4b6
Mauricio Soriano <mauricio2769@gmail.com>
2021-06-17 20:45:42 -0500

    test dump db

---
+++ app/Console/Commands/DumpMysql.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DumpMysql extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dump:mysql';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Dump and upload database on google cloud storage';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        \Spatie\DbDumper\Databases\MySql::create()
+        ->setDbName(env('DB_DATABASE'))->setUserName(env('DB_USERNAME'))
+        ->setPassword(env('DB_PASSWORD'))
+        ->dumpToFile(env('APP_ROUTE','').'storage/dump_db/dump_'.date('Y-m-d').'.sql');
+
+        \Log::info("Base de datos creada...".date('Y-m-d'));
+        /*
+        $disk = \Storage::disk('gcs');
+        $disk->put("DB_dotech.sql",\File::get(storage_path('dump_db/dump_'.date('Y-m-d').'.sql')));
+
+        \App\BinnacleImage::where('image',null)->delete();
+
+        \Log::info("Base de datos almacenada...");
+        */
+
+    }
+}
