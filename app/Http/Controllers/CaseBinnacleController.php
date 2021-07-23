9d9215808846b22450159c32fd19e2f8978b4c1d
Mauricio Soriano <mauricio2769@gmail.com>
2021-07-22 20:17:27 -0500

    hotfix

--- app/Http/Controllers/CaseBinnacleController.php
+++ app/Http/Controllers/CaseBinnacleController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\models\Caze;
 
 class CaseBinnacleController extends Controller
 {
     public function index($id){
-        $case = Caze::findOrFail($id);
+        $case =  \App\Models\Caze::findOrFail($id);
         return view('case_binnacle.index',['case' => $case]);
     }
 }
